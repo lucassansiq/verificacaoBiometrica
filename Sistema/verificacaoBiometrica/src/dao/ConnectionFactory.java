6a98cc1f9051d0777ff8f217d8233dbc5ce355a5
Lucas Santos <lucas.san12@outlook.com.br>
2021-09-28 08:40:46 -0300

    Commitando estruturação do projeto. Banco e aplicação

---
+++ Sistema/verificacaoBiometrica/src/dao/ConnectionFactory.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Lucas Hype
+ */
+public class ConnectionFactory {
+    
+    private static final String URL = "jdbc:sqlite:C:\\Users\\Lucas\\Documents\\Estudos\\APS\\APS - Biometria\\Projeto\\verificacaoBiometrica\\Sistema\\verificacaoBiometrica\\db\\verificacaoBiometrica.db";
+    
+    public static Connection getConnection(){
+      try{
+          return DriverManager.getConnection(URL);
+      }catch (SQLException e){
+          throw new RuntimeException("Erro de conexão");
+      }
+      
+    }
+    
+    public static void closeConnection(Connection con){
+        try{
+            if(con != null) con.close();
+        }catch (SQLException e){
+            throw new RuntimeException("Erro de fechamento de conexao");
+        }
+    }
+    
+    public static void closeConnection(Connection con,PreparedStatement pst){
+        try{
+            if(pst != null) pst.close();
+            closeConnection(con);
+        }catch (SQLException e){
+            throw new RuntimeException("Erro de fechamento de conexao");
+        }
+    }
+    
+    public static void closeConnection(Connection con,PreparedStatement pst,ResultSet rs){
+        try{
+            if(rs != null) rs.close();
+            closeConnection(con,pst);
+        }catch (SQLException e){
+            throw new RuntimeException("Erro de fechamento de conexao");
+        }
+    }
+}
